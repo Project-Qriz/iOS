d717cbe44b46d314668235b285df5b91554ef3b3
Hun <ksh26271@naver.com>
2024-12-22 18:29:12 +0900

    feat: LoginInputView UI 구현

--- QRIZ/Feature/Login/View/LoginMainView.swift
+++ QRIZ/Feature/Login/View/LoginMainView.swift
@@ -14,12 +14,16 @@ final class LoginMainView: UIView {
     private enum Metric {
         static let logoViewTopOffset: CGFloat = 48.0
         static let logoViewHorizontalMargin: CGFloat = 64.0
+        
+        static let inputViewTopOffset: CGFloat = 32.0
+        static let inputViewHorizontalMargin: CGFloat = 18.0
     }
     
     
     // MARK: - Properties
     
     private let loginLogoView = LoginLogoView()
+    private let loginInputView = LoginInputView()
     
     // MARK: - initialize
     
@@ -45,16 +49,24 @@ final class LoginMainView: UIView {
 
 extension LoginMainView {
     private func addSubviews() {
-        addSubview(loginLogoView)
+        [
+            loginLogoView,
+            loginInputView
+        ].forEach(addSubview(_:))
     }
     
     private func setupConstraints() {
         loginLogoView.translatesAutoresizingMaskIntoConstraints = false
+        loginInputView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             loginLogoView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: Metric.logoViewTopOffset),
             loginLogoView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Metric.logoViewHorizontalMargin),
             loginLogoView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Metric.logoViewHorizontalMargin),
+
+            loginInputView.topAnchor.constraint(equalTo: self.loginLogoView.bottomAnchor, constant: Metric.inputViewTopOffset),
+            loginInputView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Metric.inputViewHorizontalMargin),
+            loginInputView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Metric.inputViewHorizontalMargin),
         ])
     }
 }
