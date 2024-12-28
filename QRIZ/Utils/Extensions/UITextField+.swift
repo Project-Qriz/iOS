7170bc0114330253c1bdbfee2638169440b6f837
Hun-322 <ksh26271@naver.com>
2024-12-29 00:06:38 +0900

    feat: UITextField 텍스트 변경 시 발행되는 퍼블리셔 extension 추가

---
+++ QRIZ/Utils/Extensions/UITextField+.swift
@@ -0,0 +1,18 @@
+//
+//  UITextField+.swift
+//  QRIZ
+//
+//  Created by 김세훈 on 12/29/24.
+//
+
+import UIKit
+import Combine
+
+extension UITextField {
+    /// `UITextField 텍스트 변경 시 발행되는 퍼블리셔입니다.`
+    var textPublisher: AnyPublisher<String, Never> {
+        NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: self)
+            .map { _ in self.text ?? "" }
+            .eraseToAnyPublisher()
+    }
+}
