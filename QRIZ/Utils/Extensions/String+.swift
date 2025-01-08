49840f05343fc06010f8c099713d78492a5d5e69
Hun <ksh26271@naver.com>
2025-01-08 17:14:52 +0900

    feat: 문자열 유효성 검증 계산 프로퍼티 주석 수정 및 이메일 유효성 추가

--- QRIZ/Utils/Extensions/String+.swift
+++ QRIZ/Utils/Extensions/String+.swift
@@ -8,21 +8,39 @@
 import Foundation
 
 extension String {
-    /// 아이디 유효성 체크 (8~30자 + 영문 + 숫자만 가능)
+    /// 아이디 유효성 체크 조건:
+    /// 1. 길이: 2~20자
+    /// 2. 영문 대소문자 및 숫자만 사용 가능
     var isValidID: Bool {
-        let idRegex = "^[A-Za-z0-9]{8,30}$"
+        let idRegex = "^[a-zA-Z0-9]{2,20}$"
         return NSPredicate(format: "SELF MATCHES %@", idRegex).evaluate(with: self)
     }
     
-    /// 비밀번호 유효성 체크 (8~30자 + 영문 대소문자, 숫자, 특수문자만 가능)
+    /// 비밀번호 유효성 체크 조건:
+    /// 1. 길이: 8~16자
+    /// 2. 최소 1개 이상의 대문자 포함
+    /// 3. 최소 1개 이상의 소문자 포함
+    /// 4. 최소 1개 이상의 숫자 포함
+    /// 5. 최소 1개 이상의 특수문자 포함
     var isValidPassword: Bool {
-        let passwordRegex = "^[A-Za-z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]{8,30}$"
+        let passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=*!])(?=\\S+$).{8,16}$"
         return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: self)
     }
     
-    /// 이름 유효성 체크 (1~20자 + 한글/영문만 가능)
+    /// 이름 유효성 체크 조건:
+    /// 1. 길이: 1~20자
+    /// 2. 한글 또는 영문만 사용 가능
     var isValidName: Bool {
-        let nameRegex = "^[가-힣A-Za-z]{1,20}$"
+        let nameRegex = "^[a-zA-Z가-힣]{1,20}$"
+        return NSPredicate(format: "SELF MATCHES %@", nameRegex).evaluate(with: self)
+    }
+    
+    /// 이메일 유효성 체크 조건:
+    /// 1. 이메일 아이디 부분: 영문 대소문자와 숫자만 허용, 최소 2자, 최대 10자
+    /// 2. 도메인 이름 부분: 영문 대소문자와 숫자만 허용, 최소 2자, 최대 6자
+    /// 3. 최상위 도메인 부분: 영문 대소문자만 허용, 최소 2자, 최대 3자 (예: com, net, org)
+    var isValidEmail: Bool {
+        let nameRegex = "^[a-zA-Z0-9]{2,10}@[a-zA-Z0-9]{2,6}\\.[a-zA-Z]{2,3}$"
         return NSPredicate(format: "SELF MATCHES %@", nameRegex).evaluate(with: self)
     }
 }
