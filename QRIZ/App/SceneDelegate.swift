f4d3727d8e86301de6fcd9020e66fd314f42e5b9
Hun <ksh26271@naver.com>
2025-01-08 16:25:29 +0900

    feat: 네비게이션 바에 공통 뒤로가기 버튼 UI 구현 및 EmailInputViewModel 파일 생성

--- QRIZ/App/SceneDelegate.swift
+++ QRIZ/App/SceneDelegate.swift
@@ -14,7 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let name = NameInputViewController()
+        UINavigationBar.configureNavigationBackButton()
+        let name = NameInputViewController(nameInputVM: NameInputViewModel())
         let email = EmailInputViewController()
         let code = VerificationCodeViewController()
         let id = IdInputViewController()
---
+++ QRIZ/Utils/Extensions/UINavigationBar+.swift
@@ -0,0 +1,30 @@
+//
+//  UINavigationBar+.swift
+//  QRIZ
+//
+//  Created by 김세훈 on 1/8/25.
+//
+
+import UIKit
+
+extension UINavigationBar {
+    /// `전체 네비게이션 뒤로가기 버튼을 설정해주는 함수입니다.`
+    static func configureNavigationBackButton() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .white
+        appearance.backButtonAppearance.normal.titleTextAttributes = [
+            .foregroundColor: UIColor.clear,
+            .font: UIFont.systemFont(ofSize: 0.0)
+        ]
+        
+        if let backImage = UIImage(systemName: "chevron.left")?.withTintColor(.black, renderingMode: .alwaysOriginal) {
+            let offsetImage = backImage.withAlignmentRectInsets(UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 10))
+            appearance.setBackIndicatorImage(offsetImage, transitionMaskImage: offsetImage)
+        }
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+}
