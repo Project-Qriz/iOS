d59a85c61741cfeb07328c140866c3646484958b
ch <ckdgus1120@gmail.com>
2025-01-18 18:46:45 +0900

    REFACTOR: 모델 이름 ConceptCategory로 변경, 각 enum에 전체 추가, 개념만을 포함한 문자열 리턴하는 함수 제공

--- QRIZ/Utils/Model/ConceptCategory.swift
+++ QRIZ/Utils/Model/ConceptCategory.swift
@@ -7,25 +7,37 @@
 
 import Foundation
 
-enum TotalConcept: CaseIterable {
-
+enum ConceptCategory: CaseIterable {
+    
+    static func getConceptList<T: CaseIterable & RawRepresentable>(for category: T.Type) -> [String] where T.RawValue == String {
+        var arr: [String] = []
+        for elem in category.allCases {
+            arr.append(elem.rawValue)
+        }
+        arr.removeFirst()
+        return arr
+    }
+    
     enum DataModeling: String, CaseIterable {
+        case total = "전체"
         case understandOfDataModel = "데이터 모델의 이해"
         case entity = "엔터티"
         case attribute = "속성"
         case relation = "관계"
         case identifer = "식별자"
     }
-
+    
     enum DataModelAndSQL: String, CaseIterable {
+        case total = "전체"
         case normalization = "정규화"
         case relationAndJoin = "관계와 조인의 이해"
         case understandOfTransaction = "모델이 표현하는 트랜잭션의 이해"
         case understandOfNull = "NULL 속성의 이해"
         case naturalAndSurrogate = "본질식별자 vs 인조식별자"
     }
-
+    
     enum SQLBasic: String, CaseIterable {
+        case total = "전체"
         case rdbms = "관계형 데이터베이스 개요"
         case selectQuery = "SELECT문"
         case function = "함수"
@@ -35,8 +47,9 @@ enum TotalConcept: CaseIterable {
         case joinQuery = "조인"
         case standardJoin = "표준 조인"
     }
-
+    
     enum SQLAdvanced: String, CaseIterable {
+        case total = "전체"
         case subQuery = "서브 쿼리"
         case unionOperator = "집합 연산자"
         case groupFunction = "그룹 함수"
@@ -46,8 +59,9 @@ enum TotalConcept: CaseIterable {
         case pivotAndUnpivot = "PIVOT절과 UNPIVOT절"
         case regularExpression = "정규 표현식"
     }
-
+    
     enum SQLCommands: String, CaseIterable {
+        case all = "전체"
         case dml = "DML"
         case tcl = "TCL"
         case ddl = "DDL"
