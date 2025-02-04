2b7341eb63e5c18a6986c631ac0625997499c39b
ch <ckdgus1120@gmail.com>
2025-02-04 23:21:30 +0900

    config: target, minimum deployment 16.0으로 변경

---
+++ QRIZ/Feature/WrongQuestion/Components/WrongQuestionCategoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WrongQuestionCategoryCollectionViewCell.swift
+//  QRIZ
+//
+//  Created by ch on 2/2/25.
+//
+
+import UIKit
+
+class WrongQuestionCategoryCollectionViewCell: UICollectionViewCell {
+    
+}
