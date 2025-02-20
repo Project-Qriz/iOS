4e1a767a9eb4a03845db97faa4a8bec74593e1f2
Hun <ksh26271@naver.com>
2025-02-20 23:55:57 +0900

    feat: AppCoordinator 기본 구조 세팅

--- QRIZ/Coordinator/AppCoordinator.swift
+++ QRIZ/Coordinator/AppCoordinator.swift
@@ -11,3 +11,36 @@ import UIKit
 protocol Coordinator: AnyObject {
     func start() -> UIViewController
 }
+
+@MainActor
+protocol AppCoordinator: Coordinator {
+    var window: UIWindow { get set }
+}
+
+@MainActor
+protocol AppCoordinatorDependency {
+    /// 임시 선언
+//    var loginCoordinator: LoginCoordinator { get }
+//    var tabBarCoordinator: TabBarCoordinator { get }
+}
+
+@MainActor
+final class AppCoordinatorImp: AppCoordinator {
+    
+    var window: UIWindow
+    private let dependency: AppCoordinatorDependency
+    var childCoordinators: [Coordinator] = []
+    
+    init(window: UIWindow, dependency: AppCoordinatorDependency) {
+        self.window = window
+        self.dependency = dependency
+    }
+    
+    func start() -> UIViewController {
+        /// 임시 선언 추후 로그인 분기 처리 필요
+        let login = LoginViewController(loginVM: LoginViewModel())
+        window.rootViewController = login
+        window.makeKeyAndVisible()
+        return login
+    }
+}
