b4c5bcdced4d4af3ba62e48a6411dd8dc06b636d
Hun <ksh26271@naver.com>
2025-03-04 23:33:28 +0900

    feat: SendEmail API, request, service 구현

---
+++ Network/DTOs/Auth/EmailSendRequest.swift
@@ -0,0 +1,44 @@
+//
+//  EmailSendRequest.swift
+//  QRIZ
+//
+//  Created by 김세훈 on 3/4/25.
+//
+
+import Foundation
+
+// 성공
+//{
+//    "code": 1,
+//    "msg": "이메일 전송이 요청되었습니다.",
+//    "data": "인증번호가 발송되었습니다."
+//}
+// 실패
+//{
+//    "timestamp": "2025-03-04T06:58:21.717+00:00",
+//    "status": 400,
+//    "error": "Bad Request",
+//    "path": "/api/email-send"
+//}
+
+struct EmailSendRequest: Request {
+    typealias Response = EmailSendResponse
+    
+    let path = "/api/email-send"
+    let method: HTTPMethod = .post
+    let email: String
+    
+    var query: QueryItems {
+        ["email": email]
+    }
+    
+    var headers: HTTPHeader {
+        [HTTPHeaderField.contentType.rawValue: ContentType.json.rawValue]
+    }
+}
+
+struct EmailSendResponse: Decodable {
+    let code: Int
+    let msg: String
+    let data: String
+}
