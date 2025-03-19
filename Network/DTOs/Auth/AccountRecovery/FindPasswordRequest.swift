fddd2d8142c99310ec08a484439e80774fc773cc
Hun <ksh26271@naver.com>
2025-03-19 22:10:45 +0900

    feat: 비밀번호 찾기와 관련 모든 API DTO 선언 및 AccountRecovery 폴더 분리

---
+++ Network/DTOs/Auth/AccountRecovery/FindPasswordRequest.swift
@@ -0,0 +1,47 @@
+//
+//  FindPasswordRequest.swift
+//  QRIZ
+//
+//  Created by 김세훈 on 3/19/25.
+//
+
+/*
+ 실패
+ 
+ {
+     "code": -1,
+     "msg": "해당 이메일로 등록된 계정이 없습니다.",
+     "data": null
+ }
+ 
+ 성공
+ 
+ {
+     "code": 1,
+     "msg": "비밀번호 재설정 인증번호가 이메일로 발송되었습니다.",
+     "data": null
+ }
+ */
+
+import Foundation
+
+struct FindPasswordRequest: Request {
+    typealias Response = FindPasswordResponse
+    
+    let path = "/api/find-pwd"
+    let method: HTTPMethod = .post
+    let email: String
+    
+    var query: QueryItems {
+        ["email": email]
+    }
+    
+    var headers: HTTPHeader {
+        [HTTPHeaderField.contentType.rawValue: ContentType.json.rawValue]
+    }
+}
+
+struct FindPasswordResponse: Decodable {
+    let code: Int
+    let msg: String
+}
