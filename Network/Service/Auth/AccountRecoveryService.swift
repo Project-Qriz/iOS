1db87c29d43e4974db7b20baaab736b7a3db4d82
Hun <ksh26271@naver.com>
2025-03-23 22:36:19 +0900

    feat: AccountRecoveryService 비밀번호 복구 관련 API 구현

--- Network/Service/Auth/AccountRecoveryService.swift
+++ Network/Service/Auth/AccountRecoveryService.swift
@@ -12,6 +12,15 @@ protocol AccountRecoveryService {
     
     /// 아이디 찾기
     func findID(email: String) async throws -> FindIDResponse
+    
+    /// 비밀번호 찾기
+    func findPassword(email: String) async throws -> FindPasswordResponse
+    
+    /// 비밀번호 초기화 인증번호 검증
+    func verifyPasswordReset(authNumber: String) async throws -> VerifyPasswordResetResponse
+    
+    /// 비밀번호 초기화
+    func resetPassword(password: String) async throws -> PasswordResetResponse
 }
 
 final class AccountRecoveryServiceImpl: AccountRecoveryService {
@@ -30,6 +39,21 @@ final class AccountRecoveryServiceImpl: AccountRecoveryService {
     
     func findID(email: String) async throws -> FindIDResponse {
         let request = FindIDRequest(email: email)
+        return try await network.send(request)
+    }
+    
+    func findPassword(email: String) async throws -> FindPasswordResponse {
+        let request = FindPasswordRequest(email: email)
+        return try await network.send(request)
+    }
+    
+    func verifyPasswordReset(authNumber: String) async throws -> VerifyPasswordResetResponse {
+        let request = VerifyPasswordResetRequest(authNumber: authNumber)
+        return try await network.send(request)
+    }
+    
+    func resetPassword(password: String) async throws -> PasswordResetResponse {
+        let request = PasswordResetRequest(password: password)
         return try await network.send(request)
     }
 }
