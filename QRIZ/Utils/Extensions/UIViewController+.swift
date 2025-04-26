8f1d44f93c0d82e5faa1d472cc76256b6669b9fa
ch <ckdgus1120@gmail.com>
2025-04-26 21:57:12 +0900

    style: showOneButtonAlert 메서드 내부 description 오탈자 수정

--- QRIZ/Utils/Extensions/UIViewController+.swift
+++ QRIZ/Utils/Extensions/UIViewController+.swift
@@ -69,10 +69,10 @@ extension UIViewController {
     /// `에러 메시지를 표시용 원버튼 얼럿을 보여주는 함수입니다.`
     /// - Parameters:
     ///   - title: 얼럿에 표시할 에러 제목
-    ///   - descrption: 얼럿에 표시할 에러 설명
+    ///   - description: 얼럿에 표시할 에러 설명
     ///   - cancellables: 구독을 저장할 AnyCancellable
-    func showOneButtonAlert(with title: String, for descrption: String? = nil, storingIn cancellables: inout Set<AnyCancellable>) {
-        let oneButtonAlert = OneButtonCustomAlertViewController(title: title, description: descrption)
+    func showOneButtonAlert(with title: String, for description: String? = nil, storingIn cancellables: inout Set<AnyCancellable>) {
+        let oneButtonAlert = OneButtonCustomAlertViewController(title: title, description: description)
         oneButtonAlert.confirmButtonTappedPublisher
             .sink { [weak oneButtonAlert] _ in
                 oneButtonAlert?.dismiss(animated: true)
