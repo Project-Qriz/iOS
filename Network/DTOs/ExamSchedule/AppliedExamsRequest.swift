768846b306f3e653336d658c95a7be50b1f1338d
Hun <ksh26271@naver.com>
2025-05-01 23:44:26 +0900

    feat: 내 시험 일정 조회를 위한 서비스 및 API 구현

---
+++ Network/DTOs/ExamSchedule/AppliedExamsRequest.swift
@@ -0,0 +1,35 @@
+//
+//  AppliedExamsRequest.swift
+//  QRIZ
+//
+//  Created by 김세훈 on 5/1/25.
+//
+
+import Foundation
+
+struct AppliedExamsRequest: Request {
+    typealias Response = AppliedExamsResponse
+
+    let accessToken: String
+    let path = "/api/v1/applied"
+    let method: HTTPMethod = .get
+
+    var headers: HTTPHeader {
+        [
+            HTTPHeaderField.contentType.rawValue: ContentType.json.rawValue,
+            HTTPHeaderField.authorization.rawValue: accessToken
+        ]
+    }
+}
+
+struct AppliedExamsResponse: Decodable {
+    let code: Int
+    let msg: String
+    let data: ExamInfo?
+
+    struct ExamInfo: Decodable {
+        let examName: String
+        let period: String
+        let examDate: String
+    }
+}
