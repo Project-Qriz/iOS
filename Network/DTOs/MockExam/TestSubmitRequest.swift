432211cfef60fb14b62da686783357ee7d1b7f34
ch <ckdgus1120@gmail.com>
2025-05-03 14:49:16 +0900

    feat: TestSubmitRequest 구현

--- Network/DTOs/MockExam/TestSubmitRequest.swift
+++ Network/DTOs/MockExam/TestSubmitRequest.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+struct TestSubmitRequest: Request {
+    
+    // MARK: - Properties
+    typealias Response = TestSubmitResponse
+
+    private let accessToken: String
+    let path = "/api/v1/exam/submit"
+    let method: HTTPMethod = .post
+    private let testSubmitData: [TestSubmitData]
+
+    var body: Encodable? {
+        [
+            "activities": testSubmitData
+        ]
+    }
+    
+    var headers: HTTPHeader {
+        [
+            HTTPHeaderField.contentType.rawValue: ContentType.json.rawValue,
+            HTTPHeaderField.authorization.rawValue: accessToken
+        ]
+    }
+    
+    // MARK: - Initializers
+    init(accessToken: String, testSubmitData: [TestSubmitData]) {
+        self.accessToken = accessToken
+        self.testSubmitData = testSubmitData
+    }
+}
+
+struct TestSubmitResponse: Decodable {
+    let code: Int
+    let msg: String
+    let data: String? // **** 확인 필요 ****
+}
