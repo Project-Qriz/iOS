8e7002ae956f3241a576cf28a3deed91d12d116d
ch <ckdgus1120@gmail.com>
2025-05-03 12:59:13 +0900

    feat: ExamListRequest 및 ExamStatus 모델 구현

---
+++ Network/DTOs/ExamListRequest.swift
@@ -0,0 +1,54 @@
+//
+//  ExamListRequest.swift
+//  QRIZ
+//
+//  Created by ch on 5/2/25.
+//
+
+import Foundation
+
+struct ExamListRequest: Request {
+    
+    // MARK: - Properties
+    typealias Response = ExamListResponse
+    private let accessToken: String
+    
+    let path = "/api/v1/exam/session-list"
+    let method: HTTPMethod = .get
+    let examStatus: ExamStatus?
+    let isAscSortedByDate: Bool
+    var query: QueryItems {
+        var dic: [String: String] = [:]
+
+        if isAscSortedByDate { dic["sort"] = "asc" }
+
+        if let examStatus = examStatus { dic["status"] = examStatus.rawValue }
+
+        return dic
+    }
+    
+    var headers: HTTPHeader {
+        [
+            HTTPHeaderField.accessToken.rawValue: accessToken
+        ]
+    }
+    
+    // MARK: - Initializers
+    init(accessToken: String, examStatus: ExamStatus? = nil, isAscSortedByDate: Bool = false) {
+        self.accessToken = accessToken
+        self.examStatus = examStatus
+        self.isAscSortedByDate = isAscSortedByDate
+    }
+}
+
+struct ExamListResponse: Decodable {
+    let code: Int
+    let msg: String
+    let data: [DataInfo]
+    
+    struct DataInfo: Decodable {
+        let completed: Bool
+        let session: String
+        let totalScore: CGFloat?
+    }
+}
---
+++ QRIZ/Utils/Model/TestModel/ExamStatus.swift
@@ -0,0 +1,13 @@
+//
+//  ExamStatus.swift
+//  QRIZ
+//
+//  Created by ch on 5/3/25.
+//
+
+import Foundation
+
+enum ExamStatus: String {
+    case completed = "completed"
+    case notCompleted = "incomplete"
+}
