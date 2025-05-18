8fc5bd01338c9bc0d283b39a50a46842d066b899
ch <ckdgus1120@gmail.com>
2025-05-18 14:43:52 +0900

    remove: 목데이터 제공 메서드 제거

--- QRIZ/Feature/Daily/DailyResult/ViewModel/DailyResultViewModel.swift
+++ QRIZ/Feature/Daily/DailyResult/ViewModel/DailyResultViewModel.swift
@@ -183,32 +183,4 @@ final  class DailyResultViewModel {
             self.gradeResultList.append(GradeResult(id: $0 + 1, skillName: $1.detailType, question: $1.question, correction: $1.correction))
         }
     }
-    
-//    private func fetchMockData() {
-//        self.subjectScores[0] = 30
-//        self.subjectScores[1] = 20
-//        self.subjectScores[2] = 15
-//        self.subjectScores[3] = 15
-//        self.subjectScores[4] = 10
-//        self.subjectCount = 5
-//        self.passed = true
-//        
-//        self.gradeResultList.append(GradeResult(id: 1,
-//                                                skillName: "조인",
-//                                                question: """
-//                                                아래 테이블 T<S<R이 각각 다음과 같이 선언되었다. 
-//                                                다음 중 DELETE FROM T;를 수행한 후에 테이블 R에 남아있는 데이터로 가장 적절한 것은?
-//                                                """,
-//                                                correction: false))
-//        self.gradeResultList.append(GradeResult(id: 2, skillName: "SELECT 문", question: "다음 SQL문에서 발생할 수 있는 문제점으로 가장 적절한 것은?", correction: true))
-//        self.gradeResultList.append(GradeResult(id: 3, skillName: "조인", question: "다음과 같은 테이블 구조에서 가장 적절한 조인 방식은?", correction: true))
-//        self.gradeResultList.append(GradeResult(id: 4, skillName: "조인", question: "다음 SQL문에서 발생할 수 있는 문제점으로 가장 적절한 것은?", correction: false))
-//        self.gradeResultList.append(GradeResult(id: 5, skillName: "조인", question: "다음 두 쿼리의 결과가 다른 경우는?", correction: true))
-//        self.gradeResultList.append(GradeResult(id: 6, skillName: "조인", question: "다음 SQL문들의 실행 결과가 같은 것을 고르시오.", correction: true))
-//        self.gradeResultList.append(GradeResult(id: 7, skillName: "SELECT 문", question: "다음 중 SELF JOIN에 대한 설명으로 가장 부적절한 것은?", correction: false))
-//        
-//        self.subject2DetailResult.append(SubjectDetailData(majorItem: "SQL 기본", score: 40, minorItems: [SubjectDetailMinorItem("SELECT문", 20), SubjectDetailMinorItem("함수", 10), SubjectDetailMinorItem("WHERE절", 10)]))
-//        self.subject2DetailResult.append(SubjectDetailData(majorItem: "SQL 활용", score: 20, minorItems: [SubjectDetailMinorItem("서브쿼리", 10), SubjectDetailMinorItem("집합 연산자", 10)]))
-//        self.subject1DetailResult.append(SubjectDetailData(majorItem: "데이터 모델링의 이해", score: 10, minorItems: [SubjectDetailMinorItem("TCL", 5), SubjectDetailMinorItem("DDL", 5)]))
-//    }
 }
