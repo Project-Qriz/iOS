3daaf78af14b55b40f97401bf05e7dd28bdc5f6b
Hun <ksh26271@naver.com>
2025-06-06 22:53:05 +0900

    feat: 헤더 + 메인 셀 테두리용 CardBackgroundView 추가

---
+++ QRIZ/Feature/CommonViews/CardBackgroundView.swift
@@ -0,0 +1,32 @@
+//
+//  CardBackgroundView.swift
+//  QRIZ
+//
+//  Created by 김세훈 on 6/4/25.
+//
+
+import UIKit
+
+final class CardBackgroundView: UICollectionReusableView {
+    
+    // MARK: - Initialize
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Functions
+    
+    private func setupUI() {
+        backgroundColor = .white
+        clipsToBounds = true
+        layer.cornerRadius = 16
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.coolNeutral100.cgColor
+    }
+}
