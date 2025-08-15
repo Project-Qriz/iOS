f918eefd8b1eb0e461fef3435f1cb3eab598b444
Hun <ksh26271@naver.com>
2025-08-15 22:05:17 +0900

    config: 카카오 로그인 환경 변수 설정

--- QRIZ/App/AppDelegate.swift
+++ QRIZ/App/AppDelegate.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 import FirebaseCore
+import KakaoSDKCommon
+import KakaoSDKAuth
+import KakaoSDKUser
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        let appKey = Bundle.main.object(forInfoDictionaryKey: "KAKAO_NATIVE_APP_KEY") as? String ?? ""
+        KakaoSDK.initSDK(appKey: appKey)
         return true
     }
     
