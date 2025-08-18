b150122208adfeb40ffe70b81a497f3ee54985e0
Hun <ksh26271@naver.com>
2025-08-18 22:58:07 +0900

    feat: 소셜 로그인 API 연동을 위한 Request, Response 구조체 구현

---
+++ Network/DTOs/Auth/SocialLoginRequest.swift
@@ -0,0 +1,39 @@
+//
+//  SocialLoginRequest.swift
+//  QRIZ
+//
+//  Created by 김세훈 on 8/18/25.
+//
+
+import Foundation
+
+struct SocialLoginRequest: Request {
+    typealias Response = SocialLoginResponse
+    
+    let path = "/api/auth/social/login"
+    let method: HTTPMethod = .post
+    let provider: LoginViewModel.SocialLogin
+    let authCode: String
+    
+    var body: Encodable? {
+        [
+            "provider": provider.rawValue,
+            "authCode": authCode,
+            "platform": "ios"
+        ]
+    }
+    
+    var headers: HTTPHeader {
+        [HTTPHeaderField.contentType.rawValue: ContentType.json.rawValue]
+    }
+}
+
+struct SocialLoginResponse: Decodable {
+    let code: Int
+    let msg: String
+    let data: dataInfo
+    
+    struct dataInfo: Decodable {
+        let name: String
+    }
+}
