a96f736dd613471d5d1d4de12a7a118cf444699c
Hun <ksh26271@naver.com>
2026-01-02 18:01:28 +0900

    feat: 학습하러 가기 버튼 UI 구현

--- QRIZ/Feature/MistakeNote/ProblemExplanation/View/ProblemExplanationView.swift
+++ QRIZ/Feature/MistakeNote/ProblemExplanation/View/ProblemExplanationView.swift
@@ -19,6 +19,7 @@ struct ProblemExplanationView: View {
 
                 VStack(spacing: 8) {
                     ProblemQuestionSectionView(data: data.questionData) // 문제 섹션
+                    
                     ProblemResultView(
                         correctAnswer: data.answer,
                         userAnswer: data.checked ?? 0
@@ -34,13 +35,36 @@ struct ProblemExplanationView: View {
                     keyConcepts: data.keyConcepts,
                     subject: data.title
                 ) // 활용된 개념 섹션
+
+                learnButton // 학습하러가기 버튼
             }
             .padding(.horizontal, 18)
             .padding(.top, 16)
+            .padding(.bottom, 12)
         }
         .background(Color.customBlue50)
         .navigationTitle("오답노트")
         .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+// MARK: - Subviews
+
+private extension ProblemExplanationView {
+
+    /// 학습하러가기 버튼
+    var learnButton: some View {
+        Button(action: {
+            // TODO: 학습하러가기 동작 구현
+        }) {
+            Text("학습하러 가기")
+                .font(.system(size: 16, weight: .semibold))
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .frame(height: 48)
+                .background(Color(.customBlue500))
+                .cornerRadius(8)
+        }
     }
 }
 
