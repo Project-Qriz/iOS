d8a81879b3c7122f48392f20d12f866778e51d41
Hun <ksh26271@naver.com>
2026-02-18 03:20:04 +0900

    refactor: UITextField publisher에서 self 강한 참조를 weak self로 변경

--- Packages/QRIZUtils/Sources/QRIZUtils/Extensions/UITextField+.swift
+++ Packages/QRIZUtils/Sources/QRIZUtils/Extensions/UITextField+.swift
@@ -12,14 +12,14 @@ public extension UITextField {
     /// `UITextField 텍스트 변경 시 발행되는 퍼블리셔입니다.`
     var textPublisher: AnyPublisher<String, Never> {
         NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: self)
-            .map { _ in self.text ?? "" }
+            .compactMap { [weak self] _ in self?.text ?? "" }
             .eraseToAnyPublisher()
     }
 
     /// `UITextField 텍스트 입력 완료 시 발행되는 퍼블리셔입니다.`
     var textDidEndEditingPublisher: AnyPublisher<String, Never> {
         NotificationCenter.default.publisher(for: UITextField.textDidEndEditingNotification, object: self)
-            .map { _ in self.text ?? "" }
+            .compactMap { [weak self] _ in self?.text ?? "" }
             .eraseToAnyPublisher()
     }
 }
