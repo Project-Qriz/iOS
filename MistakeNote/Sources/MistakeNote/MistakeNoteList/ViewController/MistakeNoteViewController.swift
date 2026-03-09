07c0854f37e9239354bdfe5178aa2141fdd1f9e1
Hun <ksh26271@naver.com>
2026-03-09 19:58:56 +0900

    refactor: MistakeNoteViewController 패키지로 이동

---
+++ MistakeNote/Sources/MistakeNote/MistakeNoteList/ViewController/MistakeNoteViewController.swift
@@ -0,0 +1,84 @@
+//
+//  MistakeNoteViewController.swift
+//  MistakeNote
+//
+//  Created by 김세훈 on 2/23/25.
+//
+
+import UIKit
+import DesignSystem
+import SwiftUI
+import Combine
+
+@MainActor
+public protocol MistakeNoteViewControllerDelegate: AnyObject {
+    func mistakeNoteViewController(_ viewController: MistakeNoteViewController, didSelectClipWithId clipId: Int)
+    func mistakeNoteViewController(_ viewController: MistakeNoteViewController, didRequestExamForTab tab: MistakeNoteTab)
+}
+
+@MainActor
+public final class MistakeNoteViewController: UIHostingController<MistakeNoteMainView> {
+
+    // MARK: - Properties
+
+    public weak var delegate: MistakeNoteViewControllerDelegate?
+    private let viewModel: MistakeNoteListViewModel
+    private let input = PassthroughSubject<MistakeNoteListViewModel.Input, Never>()
+    private var cancellables = Set<AnyCancellable>()
+
+    // MARK: - Initialize
+
+    public init(viewModel: MistakeNoteListViewModel) {
+        self.viewModel = viewModel
+        let mistakeNoteView = MistakeNoteMainView(viewModel: viewModel)
+        super.init(rootView: mistakeNoteView)
+    }
+
+    @MainActor required dynamic public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - LifeCycle
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigationTitle()
+        bind()
+    }
+
+    override public func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureNavigationBar()
+    }
+
+    // MARK: - Private Methods
+
+    private func configureNavigationTitle() {
+        setNavigationBarTitle(title: "오답노트", textColor: UIColor.coolNeutral800)
+    }
+
+    private func configureNavigationBar() {
+        let appearance = UINavigationBar.defaultBackButtonStyle()
+        appearance.shadowColor = .clear
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+    }
+
+    private func bind() {
+        let output = viewModel.transform(input: input.eraseToAnyPublisher())
+
+        output
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] event in
+                guard let self = self else { return }
+                switch event {
+                case .navigateToClipDetail(let clipId):
+                    self.delegate?.mistakeNoteViewController(self, didSelectClipWithId: clipId)
+                case .navigateToExam(let tab):
+                    self.delegate?.mistakeNoteViewController(self, didRequestExamForTab: tab)
+                }
+            }
+            .store(in: &cancellables)
+    }
+}
