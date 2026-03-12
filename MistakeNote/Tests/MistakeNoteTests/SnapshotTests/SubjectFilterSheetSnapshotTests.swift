1b6225db1fdf50b27b5641a0a51c6d119d4464d3
Hun <ksh26271@naver.com>
2026-03-12 12:37:15 +0900

    test: SubjectFilterSheetSnapshotTests 추가

---
+++ MistakeNote/Tests/MistakeNoteTests/SnapshotTests/SubjectFilterSheetSnapshotTests.swift
@@ -0,0 +1,40 @@
+// MistakeNote/Tests/MistakeNoteTests/SnapshotTests/SubjectFilterSheetSnapshotTests.swift
+
+import XCTest
+import SnapshotTesting
+import SwiftUI
+@testable import MistakeNote
+import QRIZUtils
+
+@MainActor
+class SubjectFilterSheetSnapshotTests: MistakeNoteSnapshotTestCase {
+
+    private func makeVC(
+        availableConcepts: Set<String> = ["엔터티", "속성", "관계", "식별자"],
+        initialSelectedConcepts: Set<String> = []
+    ) -> UIViewController {
+        let view = SubjectFilterSheet(
+            isPresented: .constant(true),
+            availableConcepts: availableConcepts,
+            initialSubject: .one,
+            initialSelectedConcepts: initialSelectedConcepts
+        )
+        return UIHostingController(rootView: view)
+    }
+
+    func testInitialState() {
+        let vc = makeVC()
+        vc.view.frame = CGRect(origin: .zero, size: Self.deviceSize)
+        vc.view.layoutIfNeeded()
+
+        assertSnapshot(of: vc, as: .image)
+    }
+
+    func testWithSelectedConcepts() {
+        let vc = makeVC(initialSelectedConcepts: ["엔터티", "속성"])
+        vc.view.frame = CGRect(origin: .zero, size: Self.deviceSize)
+        vc.view.layoutIfNeeded()
+
+        assertSnapshot(of: vc, as: .image)
+    }
+}
