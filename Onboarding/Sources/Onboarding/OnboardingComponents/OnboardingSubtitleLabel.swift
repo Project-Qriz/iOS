17b2fc01fa1b07a1c54debfe8cc45003fbfa83cd
Hun <ksh26271@naver.com>
2026-03-16 22:14:01 +0900

    feat: OnboardingComponents 파일 이동

---
+++ Onboarding/Sources/Onboarding/OnboardingComponents/OnboardingSubtitleLabel.swift
@@ -0,0 +1,31 @@
+import UIKit
+import DesignSystem
+
+final class OnboardingSubtitleLabel: UILabel {
+
+    // MARK: - Initializers
+    init(_ labelText: String) {
+        super.init(frame: .zero)
+        self.attributedText = formattedText(labelText)
+        self.textColor = .coolNeutral500
+        self.font = .systemFont(ofSize: 16) // need font
+        self.textAlignment = .left
+        self.numberOfLines = 2
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("no initializer for coder: OnboardingSubtitleLabel")
+    }
+
+    // MARK: - Methods
+    private func formattedText(_ text: String) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: text)
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 4
+
+        attributedString.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
+
+        return attributedString
+    }
+}
---
+++ Onboarding/Sources/Onboarding/OnboardingComponents/OnboardingTitleLabel.swift
@@ -0,0 +1,18 @@
+import UIKit
+import DesignSystem
+
+final class OnboardingTitleLabel: UILabel {
+
+    init(labelText: String, fontSize: CGFloat = 28, numberOfLines: Int = 2) {
+        super.init(frame: .zero)
+        self.text = labelText
+        self.textColor = .coolNeutral800
+        self.font = .systemFont(ofSize: fontSize, weight: .bold)
+        self.textAlignment = .left
+        self.numberOfLines = numberOfLines
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("no initializer for coder: OnboardingTitleLabel")
+    }
+}
