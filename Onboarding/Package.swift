a1159d94be1ccff46683ac547abd3b295d926258
Hun <ksh26271@naver.com>
2026-03-16 22:10:47 +0900

    feat: Onboarding 패키지 스캐폴딩

---
+++ Onboarding/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Onboarding",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(name: "Onboarding", targets: ["Onboarding"]),
+    ],
+    dependencies: [
+        .package(path: "../Network"),
+        .package(path: "../DesignSystem"),
+        .package(path: "../QRIZUtils"),
+        .package(path: "../ExamKit"),
+    ],
+    targets: [
+        .target(
+            name: "Onboarding",
+            dependencies: [
+                "Network",
+                "DesignSystem",
+                "QRIZUtils",
+                "ExamKit",
+            ]
+        ),
+    ]
+)
