58e8f2a96b8391306cfa92cf59cca1f56121120e
Hun <ksh26271@naver.com>
2026-03-16 22:14:26 +0900

    feat: BeginOnboarding 파일 이동

---
+++ Onboarding/Sources/Onboarding/BeginOnboarding/ViewModel/BeginOnboardingViewModel.swift
@@ -0,0 +1,32 @@
+import Foundation
+import Combine
+
+final class BeginOnboardingViewModel {
+
+    // MARK: - Input & Output
+    enum Input {
+        case didButtonClicked
+    }
+
+    enum Output {
+        case moveToCheckConcept
+    }
+
+    // MARK: - Properties
+    private let output: PassthroughSubject<Output, Never> = .init()
+    private var subscriptions = Set<AnyCancellable>()
+
+    // MARK: - Methods
+    func transform(input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
+        input.sink { [weak self] event in
+            guard let self = self else { return }
+            switch event {
+            case .didButtonClicked:
+                self.output.send(.moveToCheckConcept)
+            }
+        }
+        .store(in: &subscriptions)
+
+        return output.eraseToAnyPublisher()
+    }
+}
