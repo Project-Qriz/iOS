2715f4673a2d4c8b8c144f7f86ba5365946e922e
Hun-0322 <ksh26271@naver.com>
2026-05-26 23:17:05 +0900

    add: 플랜 비활성화 아이콘 추가

--- Modules/Core/DesignSystem/Sources/DesignSystem/Extensions/PlanOption+Icon.swift
+++ Modules/Core/DesignSystem/Sources/DesignSystem/Extensions/PlanOption+Icon.swift
@@ -9,4 +9,12 @@ public extension PlanOption {
         case .thirtyDay: return .planIcon30Day
         }
     }
+
+    var disabledIcon: UIImage {
+        switch self {
+        case .sevenDay: return .planIcon7DayDisabled
+        case .fourteenDay: return .planIcon14DayDisabled
+        case .thirtyDay: return .planIcon30DayDisabled
+        }
+    }
 }
